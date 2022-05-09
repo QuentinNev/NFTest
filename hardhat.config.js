6f1686493f878978e20ebce33bc13b2ddd604146
Quentin Neves <neves.quentin@gmail.com>
2022-05-09 15:24:50 +0200

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
-};
+   solidity: "0.8.1",
+   defaultNetwork: "ropsten",
+   networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+  },
+}
